96aebe9f3fc6610f31d82ce357addd4d0c839233
Atharv Pote <atharvpote@gmail.com>
2022-07-04 13:03:11 +0530

    Initial commit.

---
+++ src/utils/colors.js
@@ -0,0 +1,5 @@
+export const neutrals = {
+  almostWhite: "hsl(0, 0%, 98%)",
+  mediumGrey: "hsl(0, 0%, 41%)",
+  almostBlack: "hsl(0, 0%, 8%)",
+};
---
+++ src/utils/typeScale.js
@@ -0,0 +1,20 @@
+export const baseFontSize = "18px";
+
+export const fontFamily = "'Epilogue', sans-serif";
+
+export const typeScale = {
+  heading1: "3.052rem",
+  heading2: "2.441rem",
+  heading3: "1.953rem",
+  heading4: "1.563rem",
+  heading5: "1.25rem",
+  paragraph1: "1rem",
+  helperText: "0.8rem",
+  copyrightText: "0.64rem",
+  disclaimerText: "0.512rem",
+};
+
+export const weightScale = {
+  medium: 500,
+  bold: 700,
+};
